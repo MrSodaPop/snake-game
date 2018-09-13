d53e2d2c9e0772bc379a37d89931a625293c4921
Joel L <joelmleifer@gmail.com>
2018-09-13 16:21:11 +1000

    Initial Commit

---
+++ Snake/javascript/index.js
@@ -0,0 +1,107 @@
+$(document).ready(function() {
+    console.log("Document ready");
+    generateGameArea(20);
+    gameData = generateGameData();
+
+    $('html').keypress(function(key){switch (parseInt(key.keyCode)){
+      case 97: if (gameData.direction == 'left' || gameData.direction == 'right' || gameData.changeDirection == true){return;}else{gameData.newDirection = 'left'; gameData.changeDirection = true;} break;
+      case 119: if (gameData.direction == 'up' || gameData.direction == 'down' || gameData.changeDirection == true){return;}else{gameData.newDirection = 'up'; gameData.changeDirection = true;} break;
+      case 100: if (gameData.direction == 'left' || gameData.direction == 'right' || gameData.changeDirection == true){return;}else{gameData.newDirection = 'right'; gameData.changeDirection = true;} break;
+      case 115: if (gameData.direction == 'up' || gameData.direction == 'down' || gameData.changeDirection == true){return;}else{gameData.newDirection = 'down'; gameData.changeDirection = true;} break;
+      default: console.log('invalid key');
+    }});
+
+    gameUpdate();
+});
+
+var gameOver = function() {
+  console.log('game over');
+  gameData.gameOver = true;
+}
+
+var gameUpdate = function() {
+var interval = setInterval(function(){
+  if(gameData.gameOver) {
+    $('.score').replaceWith("<div class=\'score\'>Game Over - " + (gameData.activeSquares.length-1) + "</div>");
+    $('.score').css('font-size','3vw');
+    return;
+  }
+  if (gameData.changeDirection) {
+    gameData.direction = gameData.newDirection;
+    gameData.changeDirection = false;
+  }
+  moveSnake();
+  $('.score').replaceWith("<div class=\'score\'>" + (gameData.activeSquares.length-1) + "</div>");
+},gameData.tickrate)};
+
+var moveSnake = function() {
+  let newLead = gameData.leadingSquare;
+  switch(gameData.direction) {
+    case 'left': if (gameData.leadingSquare % 20 === 0) {gameOver(); return;} else { for(let i = 0;i<gameData.activeSquares.length;i++){if(gameData.leadingSquare - 1 === gameData.activeSquares[i]){gameOver();return;}else{newLead = gameData.leadingSquare - 1}}} break;
+    case 'right': if (gameData.leadingSquare % 20 === 19) {gameOver(); return;} else { for(let i = 0;i<gameData.activeSquares.length;i++){if(gameData.leadingSquare + 1 === gameData.activeSquares[i]){gameOver();return;}else{newLead = gameData.leadingSquare + 1}}} break;
+    case 'down': if (gameData.leadingSquare > 379) {gameOver(); return;} else { for(let i = 0;i<gameData.activeSquares.length;i++){if(gameData.leadingSquare + 20 === gameData.activeSquares[i]){gameOver();return;}else{newLead = gameData.leadingSquare + 20}}} break;
+    case 'up': if (gameData.leadingSquare < 21) {gameOver(); return;} else { for(let i = 0;i<gameData.activeSquares.length;i++){if(gameData.leadingSquare - 20 === gameData.activeSquares[i]){gameOver();return;}else{newLead = gameData.leadingSquare - 20}}} break;
+  }
+  var temporaryArray = [newLead];
+  for (let i = 0; i < gameData.activeSquares.length - 1; i++) {
+    temporaryArray[i+1] = gameData.activeSquares[i];
+  }
+  if (newLead === gameData.fruitSquare) {
+    temporaryArray[temporaryArray.length] = gameData.activeSquares[gameData.activeSquares.length - 1];
+    generateFruit();
+    gameData.tickrate = gameData.tickrate * 0.5;
+  }
+  else {
+    $('#' + gameData.activeSquares[gameData.activeSquares.length - 1] + "-tile").css('background-color','black');
+  }
+  gameData.activeSquares = temporaryArray;
+  gameData.leadingSquare = newLead;
+  for (let i = 0; i <gameData.activeSquares.length; i++) {
+    $('#' + gameData.activeSquares[i] + '-tile').css('background-color','#52ea3a')
+  }
+}
+
+var generateGameArea = function() {
+  for (let i = 0; i < 400; i++) {
+    $('#game-area').append("<div class=\'game-tile\' id=\'" + i + "-tile\'></div>");    
+  }
+  console.log('Game area generated');
+}
+
+var generateFruit = function() {
+  var overlapping = true;
+  while (overlapping) {
+    fruitSquare = Math.floor(Math.random() * 400);
+    overlapping = false;
+    for (let i = 0; i < gameData.activeSquares.length + 1; i++) {
+      if (fruitSquare === gameData.leadingSquare || fruitSquare === gameData.activeSquares[i]) {
+        overlapping = true
+      }
+    }
+  }
+  gameData.fruitSquare = fruitSquare;
+  $('#' + gameData.fruitSquare + '-tile').css('background-color','red');
+};
+
+generateGameData = function() {
+  var leadingSquare = 190;
+  var activeSquares = [leadingSquare];
+  var fruitSquare = Math.floor(Math.random() * 400);
+  while (fruitSquare === leadingSquare) {
+    fruitSquare = Math.floor(Math.random() * 400);
+  }
+  $('#190-tile').css('background-color','#52ea3a');
+  $('#' + fruitSquare + '-tile').css('background-color','red');
+  return {
+    leadingSquare: leadingSquare,
+    direction: 'right',
+    newDirection: 'right',
+    changeDirection: false,
+    activeSquares: activeSquares,
+    fruitSquare: fruitSquare,
+    tickrate: 200,
+    gameOver: false
+  }
+}
+
+var gameData;
